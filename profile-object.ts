07a7fd2a8aeef8a51051c4a8ba87eead3399145c
Mauro Cruz <52788945+maurocpc@users.noreply.github.com>
2026-01-13 12:05:42 +0000

    Create first object with type

---
+++ profile-object.ts
@@ -0,0 +1,41 @@
+type Profile = {
+    name: string;
+    age: number;
+    favoriteHobby: string;
+    isLearningToCode: boolean;
+    yearsCoding: number;
+    country: string;
+};
+
+const profile = {
+    name: "Mauro",
+    age: 50,
+    favoriteHobby: "Playing video games",
+    isLearningToCode: true,
+    yearsCoding: 0,
+    country: "Brazil"
+};
+
+function createProfileMessage(profileData: Profile): string {
+  let message = `Hi! My name is ${profileData.name}, I'm ${profileData.age}, I love ${profileData.favoriteHobby}. I'm from ${profileData.country}.`;
+
+  if (profileData.yearsCoding < 0) {
+    return "Invalid coding experience.";
+  }
+
+  if (profileData.yearsCoding === 0) {
+    message += " I'm just starting out.";
+  }
+
+  if (profileData.yearsCoding > 0) {
+    message += ` I've been coding for ${profileData.yearsCoding} years.`;
+  }
+
+  if (profileData.isLearningToCode) {
+    message += " I'm learning to code!";
+  }
+
+  return message;
+}
+
+console.log(createProfileMessage(profile));
